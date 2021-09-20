dec7d985818a83fb30469c867b5734f4b88265ef
Sasquatch0908 <89185962+SourasishBasu@users.noreply.github.com>
2021-09-20 18:15:06 +0530

    Delete main.py

--- tkinter/now_showing/main.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-from nowshowing import *
-nowshowingObject = movies()
-nowshowing.rootinclass.mainloop()
-if nowshowing.nowshowingbool == "True":
-    newwindow = Tk()
-    newwindow.mainloop()
