76585895baad43c790d2aec9e59791c7fa141fd7
Arko9699 <89677542+Arko9699@users.noreply.github.com>
2021-09-21 08:30:27 +0530

    Update card details.py
    separated month and year in expiry

--- tkinter/card details.py
+++ tkinter/card details.py
@@ -7,36 +7,41 @@ root.geometry("370x150")
 def payment():
    cardnumber = cardnumber_input.get()
    cardname = cardname_input.get()
-   expiry = expiry_input.get()
+   expirym = expirym_input.get()
+   expiryy = expiryy_input.get()
    cvv = cvv_input.get()
    cardnumber_input.delete(0, END)
    cardname_input.delete(0, END)
-   expiry_input.delete(0, END)
+   expirym_input.delete(0, END)
+   expiryy_input.delete(0, END)
    cvv_input.delete(0, END)
 
 #Input boxes
 cardnumber_input = Entry(root, width = 30)
 cardnumber_input.insert(0, "Enter the number of your card")
 cardnumber_input.grid(row = 0, column = 1, padx = 5)
+
 cardname_input= Entry(root, width = 30)
 cardname_input.insert(0, "Enter name on the card")
 cardname_input.grid(row = 1, column = 1)
-expiry_input = Entry(root, width = 30)
-expiry_input.insert(0, "MM/YY")
-expiry_input.grid(row = 2, column = 1)
+
+expirym_input = Entry(root, width = 5)
+expirym_input.insert(0, "MM")
+expirym_input.grid(row = 2, column=1, sticky=W, padx=5)
+
+expiryy_input = Entry(root, width = 5)
+expiryy_input.insert(0, "YY")
+expiryy_input.grid(row = 2, column=1, sticky=W, padx=42)
+
 cvv_input = Entry(root, width = 30)
 cvv_input.insert(0, "CVV")
 cvv_input.grid(row = 3, column = 1)
 
 #Labels for input boxes
-cardnumber_label = Label(root, text = "Card Number", padx = 40, pady = 2)
-cardnumber_label.grid(row = 0, column = 0)
-cardname_label = Label(root, text = "Name on card owner", pady = 2)
-cardname_label.grid(row = 1, column = 0)
-expiry_label = Label(root, text = "Expiry date", pady = 2)
-expiry_label.grid(row = 2, column = 0)
-cvv_label = Label(root, text = "CVV", pady = 2)
-cvv_label.grid(row = 3, column = 0)
+cardnumber_label = Label(root, text = "Card Number", padx = 40, pady = 2).grid(row = 0, column = 0)
+cardname_label = Label(root, text = "Name on card owner", pady = 2).grid(row = 1, column = 0)
+expiry_label = Label(root, text = "Expiry date", pady = 2).grid(row = 2, column = 0)
+cvv_label = Label(root, text = "CVV", pady = 2).grid(row = 3, column = 0)
 
 #Make payment button
 payment_button = Button(root, text = "Make Payment", command = payment)
